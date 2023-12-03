db03c75bc8dad381d1fa4fafcc1b19b106512ee4
GitFutureTrunks <145227152+GitFutureTrunks@users.noreply.github.com>
2023-12-03 16:38:35 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,19 @@
+fx_version 'bodacious'
+game 'gta5'
+lua54 'yes'
 
+author 'GitFutureTrunks'
+description 'Totally Wireless'
+version '2.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts{
+    'client/main.lua',
+}
+
+server_scripts {
+    'server/main.lua'
+}
